c6e926e1cbc7fe4b35b685ad245938ece39b6227
Marwin Acker <marwin.extern@funk.net>
2023-11-14 13:58:35 +0100

    implement my own testing

--- test/main.c
+++ test/main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "minunit.h"
 
-static char * test_parsing_tokens(){
-    mu_assert("error, wrong tokens", 1 == 2);
+int assert_str(char * expected, char * actual){
+    if(strcmp(expected, actual) != 0) {
+        printf("Strings not equal: expected \"%s\" but got \"%s\"", expected, actual);
+        return 1;
+    }
+    return 0;
+}
+int assert_int(int expected, int actual){
+    if(expected != actual) {
+        printf("Ints not equal: expected \"%d\" but got \"%d\"", expected, actual);
+        return 1;
+    }
+    return 0;
 }
 
+int run_tests(){
+    int val = 0;
+    val += assert_str("test", "test");
+    return val;
+}
+
+
 int main(){
-    EXIT_SUCCESS;
+    if(run_tests() == 0){
+      return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
 }
