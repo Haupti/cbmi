e96bf0cd9c50494c82bc606e6924ecd555773595
marwin <marwin.ack@googlemail.com>
2023-11-18 22:24:43 +0100

    move file reading to fileio

--- main.c
+++ main.c
@@ -4,22 +4,7 @@
 #include <unistd.h>
 #include "src/lexer.h"
 #include "src/interpreter.h"
-
-int read_file(char * filename, char * target, int max_bytes){
-    size_t size_tracker = 0;
-    FILE * fptr;
-    fptr = fopen(filename, "r");
-    char line[200];
-    while(fgets(line, 200, fptr) != NULL){
-        if(size_tracker + strlen(line) > max_bytes){
-            return -1;
-        }
-        size_tracker += strlen(line);
-        strcat(target, line);
-    }
-    fclose(fptr);
-    return 0;
-}
+#include "src/fileio.h"
 
 
 int main(int args, char * argv[]){
